4e3fac41f9590a4683798e3d6c2f5ca0d956cc0f
Omar Khairy <omarkhairy21@gmail.com>
2019-11-09 01:23:21 +0200

    Add Client Files

---
+++ front/src/actions/types.js
@@ -0,0 +1,8 @@
+export const FETCH_USER = 'fetch_user';
+export const FETCH_BLOGS = 'fetch_blogs';
+export const FETCH_BLOG = 'fetch_blog';
+export const FETCH_BLOGing = 'fetch_blogss';
+
+/**
+ * Hey sdasdnkn
+ */
