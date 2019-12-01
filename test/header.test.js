d92bf0a54193975beae867c6221db95d8e139095
mohamedkhairy <mohamed.khairy@iqraaly.com>
2019-12-01 21:45:22 +0200

    Add Session Factory

--- test/header.test.js
+++ test/header.test.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
-
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 let browser, page;
 
 beforeEach(async () => {
@@ -28,30 +29,14 @@ test('We can Lunch a browser', async () => {
 test('clicking login starts oauth flow', async () => {
   await page.click('.right a');
   const url = await page.url();
-  console.log(url);
   expect(url).toMatch(/accounts\.google\.com/)
 });
 
-test.only('When signed in, shows logout button', async () => {
-  const id = '5da4c354b9349c2e9c74f654';
+test('When signed in, shows logout button', async () => {
+  const user = await userFactory();
+  const {session, sig} = sessionFactory(user);
 
-  const Buffer = require('safe-buffer').Buffer;
-
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  }
-
-  const sessionString = Buffer.from(
-    JSON.stringify(sessionObject)
-  ).toString('base64'); 
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('express:sess=' + sessionString);
-  await page.setCookie({name:'express:sess', value: sessionString});
+  await page.setCookie({name:'express:sess', value: session});
   await page.setCookie({name: 'express:sess.sig' , value: sig});
 
   await page.goto('http://localhost:3000');
