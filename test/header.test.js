7831024e7dd0bb2720c3bf98f90d1625bbbe4caf
omar khairy <omarkhairy21@gmail.com>
2020-02-18 22:06:09 +0200

    Add Test to Review add blog post -Submitting takes user to review screen

--- test/header.test.js
+++ test/header.test.js
@@ -1,13 +1,10 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
-let browser, page;
+const Page = require('./helpers/page');
+
+let page;
 
 beforeEach(async () => {
-   browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  
+  page = await Page.build();
   await page.goto('localhost:3000');
   // page.deleteCookie({name: 'session'}).then( () => {
   // page.deleteCookie({name: 'session.sig'})
@@ -17,11 +14,11 @@ beforeEach(async () => {
 });
 
 afterEach(async ()=> {
-   await browser.close();
+   await page.close();
 })
 
-test('We can Lunch a browser', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+test('The Header Has The Correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo');
 
   expect(text).toEqual('Blogster');
 });
@@ -33,14 +30,7 @@ test('clicking login starts oauth flow', async () => {
 });
 
 test('When signed in, shows logout button', async () => {
-  const user = await userFactory();
-  const {session, sig} = sessionFactory(user);
-
-  await page.setCookie({name:'express:sess', value: session});
-  await page.setCookie({name: 'express:sess.sig' , value: sig});
-
-  await page.goto('http://localhost:3000');
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login()
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
 
   expect(text).toEqual('Logout');
