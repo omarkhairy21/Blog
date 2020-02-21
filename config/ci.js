3572a161cdbce02a1edb8922ff51925b4f6ac12b
omar khairy <omarkhairy21@gmail.com>
2020-02-21 20:41:50 +0200

    change monguri to be compatible with travis docs - add redisUrl to config: dev and ci

--- config/ci.js
+++ config/ci.js
@@ -1,6 +1,7 @@
 module.exports = {
   googleClientID: '1093743328755-arphdgii4gad10f9q3cu3f946vjh7gbe.apps.googleusercontent.com',
   googleClientSecret: 'M1LoMkzRyYpLQtOSjHlWd5kL',
-  mongoURI: 'mongodb+srv://khairy:hEXEJMisDGngU5jT@cluster0-dyyja.mongodb.net/Blog-fullstack?retryWrites=true',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl:'redis://127.0.0.1:6379'
 };
--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,8 @@ module.exports = {
   googleClientID: '1093743328755-arphdgii4gad10f9q3cu3f946vjh7gbe.apps.googleusercontent.com',
   googleClientSecret: 'M1LoMkzRyYpLQtOSjHlWd5kL',
   mongoURI: 'mongodb+srv://khairy:hEXEJMisDGngU5jT@cluster0-dyyja.mongodb.net/Blog-fullstack?retryWrites=true',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl:'redis://127.0.0.1:6379'
 };
 
 // eyJwYXNzcG9ydCI6eyJ1c2VyIjoiNWRhNGMzNTRiOTM0OWMyZTljNzRmNjU0In19
