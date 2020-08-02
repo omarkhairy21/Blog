7dc5fb17cffb0c39fe3545edce30048d055a7b65
omar khairy <omarkhairy21@gmail.com>
2020-08-02 07:25:28 +0200

    Clean up

--- test/header.test.js
+++ test/header.test.js
@@ -2,24 +2,17 @@ const Page = require('./helpers/page');
 
 let page;
 
-beforeEach(async () => {
-  
+beforeEach( async () => {
   page = await Page.build();
   await page.goto('http://localhost:3000');
-  // page.deleteCookie({name: 'session'}).then( () => {
-  // page.deleteCookie({name: 'session.sig'})
-  // }).then(() => {
-  //   console.log('All Cookies Deleted');
-  // })
 });
 
-afterEach(async ()=> {
+afterEach( async ()=> {
    await page.close();
 })
 
 test('The Header Has The Correct text', async () => {
   const text = await page.getContentsOf('a.brand-logo');
-
   expect(text).toEqual('Blogster');
 });
 
@@ -32,6 +25,5 @@ test('clicking login starts oauth flow', async () => {
 test('When signed in, shows logout button', async () => {
   await page.login()
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
-
   expect(text).toEqual('Logout');
 })
